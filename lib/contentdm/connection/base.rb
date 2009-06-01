bc6f128d655b23432c5fba27578beef42320ef75
Derek Kastner <dkastner@gmail.com>
2009-06-01 19:38:36 -0400

    Finished ContentDM::Connection::Base#path_for

--- lib/contentdm/connection/base.rb
+++ lib/contentdm/connection/base.rb
@@ -7,7 +7,7 @@ module ContentDM
       
       attr_accessor :host, :root_path
       
-      def initialize(host, root_path = '/cdm_rest', type = :rest, cache_enabled = false)
+      def initialize(host, root_path = nil, type = :rest, cache_enabled = false)
         case type
         when :rest then
           self.class.rest(host, root_path)
@@ -25,10 +25,17 @@ module ContentDM
       end
       
       class << self
-        def rest(host, root_path = '/cdm_rest')
+        def rest(host, root_path = nil)
           self.extend(ContentDM::Connection::REST::ClassMethods)
           self.send(:include, ContentDM::Connection::REST::InstanceMethods)
         end
+      end
+      
+    private
+      def path_for(*args)
+        first_item = root_path.nil? ? args.shift : root_path
+        path = (first_item =~ /^\//) ? first_item : '/' + first_item
+        ([path] + args).join('/')
       end
     end
   end
--- spec/lib/contentdm/connection/base_spec.rb
+++ spec/lib/contentdm/connection/base_spec.rb
@@ -1,10 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
 
 describe ContentDM::Connection::Base do
+  before(:each) do
+    @connection = ContentDM::Connection::Base.new('example.com', 'cdm_rest')
+  end
+  
   describe "initialize" do
-    before(:each) do
-      @connection = ContentDM::Connection::Base.new('example.com')
-    end
     it "should create a REST connection" do
       @connection.should be_a_kind_of(ContentDM::Connection::REST::InstanceMethods)
     end
@@ -12,7 +13,26 @@ describe ContentDM::Connection::Base do
       @connection.host.should == 'example.com'
     end
     it "should set the root_path attribute" do
-      @connection.root_path.should == '/cdm_rest'
+      @connection.root_path.should == 'cdm_rest'
+    end
+  end
+  
+  describe "path_for" do
+    it "should prepend the root_path if specified and prepend a slash if none is provided" do
+      @connection.root_path = 'cdm_rest'
+      @connection.send(:path_for, 'big','red').should == '/cdm_rest/big/red'
+    end
+    it "should prepend the root_path if specified and not prepend a slash if one is provided" do
+      @connection.root_path = '/cdm_rest'
+      @connection.send(:path_for,'big','red').should == '/cdm_rest/big/red'
+    end
+    it "should not prepend a root path and prepend a slash if none is provided" do
+      @connection.root_path = nil
+      @connection.send(:path_for,'big','red').should == '/big/red'
+    end
+    it "should not prepend a root path and not prepend a slash if one is provided" do
+      @connection.root_path = nil
+      @connection.send(:path_for,'/big','red').should == '/big/red'
     end
   end
 end
