cc42aa119dfb0554b9e469f0ae82b922713c3505
Derek Kastner <dkastner@gmail.com>
2009-06-01 20:56:43 -0400

    Added name attribute, #from_rest_hash method, alias= can now handle nils

--- lib/contentdm/collection.rb
+++ lib/contentdm/collection.rb
@@ -1,15 +1,24 @@
 module ContentDM
   class Collection
-    attr_accessor :alias
+    attr_accessor :alias, :name
     
-    def initialize(attrs)
+    def initialize(attrs = {})
       attrs.each do |attribute, value|
         self.send("#{attribute}=", value)
       end
     end
     
+    class << self
+      def from_rest_hash(hash)
+        collection = self.new
+        collection.name = hash['name']
+        collection.alias = hash['alias']
+        collection
+      end
+    end
+    
     def alias=(val)
-      @alias = val.strip
+      @alias = val.nil? ? nil : val.strip
     end
   end
 end
--- spec/lib/contentdm/collection_spec.rb
+++ spec/lib/contentdm/collection_spec.rb
@@ -1,12 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe ContentDM::Collection do
-  describe "all" do
-    
+  describe "from_rest_hash" do
+    it 'should convert a hash retrieved from the REST interface into a Collection' do
+      collection = ContentDM::Collection.from_rest_hash({
+        "alias" => "/p4006coll3",
+        "name" => "Civil War Photographs",
+        "path" => "D:\\Sites\\129401\\Data\\p4006coll3"
+      })
+      collection.name.should == 'Civil War Photographs'
+      collection.alias.should == "/p4006coll3"
+    end
   end
-  describe "find_by_alias" do
-    
-  end
+  
   describe "alias" do
     it "should trim any leading and trailing whitespace" do
       collection = ContentDM::Collection.new(:alias => ' /p23ann45  ')
